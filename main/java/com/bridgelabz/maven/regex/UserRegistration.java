7119499580d01a3ca1d139d89543280d8e46d379
muxthafa <musthafamohd0@gmail.com>
2021-09-10 22:00:23 +0530

    Added: custom exceptions to handle invalid details

--- main/java/com/bridgelabz/maven/regex/UserRegistration.java
+++ main/java/com/bridgelabz/maven/regex/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 /**
  * class UserRegistration contains functions
  * to validate user details using regex
- * @author mohammad.musthafa_ym
+ * @author Mohammad Musthafa
  */
 public class UserRegistration 
 {
@@ -13,13 +13,14 @@ public class UserRegistration
 	 * @method checkFirstname checks if first name matches pattern
 	 * @param firstname
 	 * @return boolean
+	 * @throws UserRegistrationException 
 	 */
-	public boolean checkFirstname(String firstname) {
-		String firstname_pattern="^[A-Z]{1}[a-zA-Z]{3,}";
-		if(Pattern.matches(firstname_pattern, firstname)) {
+	public boolean checkFirstname(String firstName) throws UserRegistrationException {
+		String firstname_pattern = "^[A-Z]{1}[a-zA-Z]{3,}";
+		if (Pattern.matches(firstname_pattern, firstName)) {
 			return true;
-		}else {
-			return false;
+		} else {
+			throw new UserRegistrationException("Enter valid first name");
 		}
 	}
 	
@@ -27,13 +28,14 @@ public class UserRegistration
 	 * @method checkLastname checks if last name matches pattern
 	 * @param lastname
 	 * @return boolean
+	 * @throws UserRegistrationException 
 	 */
-	public boolean checkLastname(String lastname) {
+	public boolean checkLastname(String lastName) throws UserRegistrationException {
 		String lastname_pattern="^[A-Z]{1}[a-zA-Z]{3,}";
-		if(Pattern.matches(lastname_pattern, lastname)) {
+		if(Pattern.matches(lastname_pattern, lastName)) {
 			return true;
 		}else {
-			return false;
+			throw new UserRegistrationException("Enter valid last name");
 		}
 	}
 	
@@ -41,13 +43,14 @@ public class UserRegistration
 	 * @method email checks if email matches pattern
 	 * @param email
 	 * @return boolean
+	 * @throws UserRegistrationException 
 	 */
-	public boolean checkEmail(String email) {
+	public boolean checkEmail(String email) throws UserRegistrationException {
 		String email_pattern="^[a-zA-Z0-9-_]+([.]?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+[.][a-zA-Z]{1,4}([.]?[a-zA-Z-_]+)";
 		if(Pattern.matches(email_pattern, email)) {
 			return true;
 		}else {
-			return false;
+			throw new UserRegistrationException("Enter valid email");
 		}
 	}
 	
@@ -55,13 +58,14 @@ public class UserRegistration
 	 * @method checkPhone checks if phone number matches pattern
 	 * @param number
 	 * @return boolean
+	 * @throws UserRegistrationException 
 	 */
-	public boolean checkPhone(String number) {
-		String number_pattern="^([+][0-9]{2,3})[ ][0-9]{10}";
+	public boolean checkPhone(String number) throws UserRegistrationException {
+		String number_pattern="^([+][0-9]{2,3})?[ ]?[0-9]{10}";
 		if(Pattern.matches(number_pattern, number)) {
 			return true;
 		}else {
-			return false;
+			throw new UserRegistrationException("Enter valid number");
 		}
 	}
 	
@@ -69,14 +73,15 @@ public class UserRegistration
 	 * @method checkPassword validates password
 	 * @param password
 	 * @return boolean
+	 * @throws UserRegistrationException 
 	 */
-	public boolean checkPassword(String password) {
+	public boolean checkPassword(String password) throws UserRegistrationException {
 		String password_pattern;
 		if(password.length()>7) {
 			password_pattern="^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
 			return Pattern.matches(password_pattern, password); 
 		}else {
-			return false;
+			throw new UserRegistrationException("Enter valid password");
 		}
 	}
 }
--- test/java/com/bridgelabz/maven/regex/UserRegistrationTest.java
+++ test/java/com/bridgelabz/maven/regex/UserRegistrationTest.java
@@ -12,57 +12,83 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import junit.framework.Assert;
+
 /**
  * Unit test for simple UserRegistration.
  */
 
-@RunWith(Parameterized.class)
-public class UserRegistrationTest 
-{
-    	String firstName="Mohammad";
-    	String lastName="Musthafa";
-    	String email;
-    	String phone_number="+91 7760219131";
-    	String password="Musthafa@26";
-    	Boolean expectedResult;
-    	UserRegistration user_details;
-    	
-    	@Before
-    	public void initialize()
-    	{
-    		user_details=new UserRegistration();
-    	}
-    	
-    	public UserRegistrationTest(String email,Boolean expectedResult)
-    	{
-    		this.email=email;
-    		this.expectedResult=expectedResult;
-    	}
-    	
-    	@Parameterized.Parameters
-    	public static Collection input()
-    	{
-    		return Arrays.asList(
-    				new Object[][] {
-    					{"mxo@gmail.com",true},
-    					{"mux-26@gmail.com",true},{"abc.17@yml.com",true},{"mux11@yml.com",true},
-    					{"mux-999@abc.net",true},{"mux.111@abc.com.au",true},
-    					{"mux@3.com",true},{"mux@gmail.com.com",true},
-    					{"abc",false},{"abc@.com.my",false},{"abc123@gmail.a",false},
-    					{"abc123@.com",false},{"abc123@.com.com",false},
-    					{".mux@abc.com",false},{"mux$3*@gmail.com",false},{"mux@%*.com",false},
-    					{"abc.@yahoo.com",false},{"mux@gmail.com.1a",false},{"mux@gmail.com.aa.au",false}
-    					});
-    	}
-    	
-    	@Test
-        public void patternTest()
-        {
-    		UserRegistration registration = new UserRegistration();
-    		assertEquals(true,registration.checkFirstname(firstName));
-    		assertEquals(true,registration.checkLastname(lastName));
-    		assertEquals(true,registration.checkEmail(email));
-    		assertEquals(true,registration.checkPhone(phone_number));
-    		assertEquals(true,registration.checkPassword(password));
-        }
+
+public class UserRegistrationTest {
+	Scanner sc = new Scanner(System.in);
+	
+	@Test
+	public void firstNameTest() {
+		UserRegistration validation = new UserRegistration();
+		boolean result;
+		System.out.println("Enter the first name");
+		String firstName = sc.next();
+		try {
+			result = validation.checkFirstname(firstName);
+			Assert.assertEquals(true, result);
+		}catch(UserRegistrationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void lastNameTest() {
+		UserRegistration validation = new UserRegistration();
+		boolean result;
+		System.out.println("Enter the last name");
+		String lastName = sc.next();
+		try {
+			result = validation.checkLastname(lastName);
+			Assert.assertEquals(true, result);
+		}catch(UserRegistrationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void emailTest() {
+		UserRegistration validation = new UserRegistration();
+		boolean result;
+		System.out.println("Enter the email");
+		String email = sc.next();
+		try {
+			result = validation.checkEmail(email);
+			Assert.assertEquals(true, result);
+		}catch(UserRegistrationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void numberTest() {
+		UserRegistration validation = new UserRegistration();
+		boolean result;
+		System.out.println("Enter the phone number");
+		String number = sc.next();
+		try {
+			result = validation.checkPhone(number);
+			Assert.assertEquals(true, result);
+		}catch(UserRegistrationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void passwordTest() {
+		UserRegistration validation = new UserRegistration();
+		boolean result;
+		System.out.println("Enter the password");
+		String password = sc.next();
+		try {
+			result = validation.checkPassword(password);
+			Assert.assertEquals(true, result);
+		}catch(UserRegistrationException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
